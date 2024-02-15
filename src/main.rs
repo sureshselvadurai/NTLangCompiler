9e85f127fa07f0f1ebe77568ee29af48781ab2d2
sureshselvadurai <sureshrajaselvadurai@gmail.com>
2024-02-14 18:46:34 -0800

    Scanner

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod config;
 mod scan;
+mod parse;
+mod ntlang;
 
 use std::env;
 use config::Config;
@@ -12,6 +14,9 @@ fn main() {
     let mut st = ScanTableSt::new();
     scan_table_scan(&mut st, config.expression.as_bytes());
     st.print();
+
+    parse_table_init(&parse_table);
+    parse_tree = parse_program(&parse_table, &scan_table);
 
     println!("Expression: {}", config.expression);
 }
